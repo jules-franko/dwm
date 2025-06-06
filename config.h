76394552d636952f225d5a106991cccc1affebbc
jules-franko <julesfranko@proton.me>
2025-06-06 01:10:42 -0600

    keybinds

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const unsigned int gappih    = 10;       /* horiz inner gap between windo
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "terminus:size=15","fontawesome" };
@@ -92,7 +92,9 @@ static const char *mail[] = {"st", "-e", "neomutt", NULL};
 static const char *findsong[] = {"/home/jules/.scripts/findsong", NULL};
 static const char *imageview[] = {"/home/jules/.scripts/imageview", NULL};
 static const char *killprocess[] = {"/home/jules/.scripts/killprocess", NULL};
+static const char *playvideo[] = {"/home/jules/.scripts/playvideo", NULL};
 static const char *passmenu[] = {"/home/jules/.scripts/passmenu", NULL};
+static const char *switchoutput[] = {"/home/jules/.scripts/switchoutput", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -111,6 +113,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_u,      spawn,          {.v = imageview } },
 	{ MODKEY,                       XK_y,      spawn,          {.v = passmenu } },
 	{ MODKEY|ControlMask,           XK_q,      spawn,          {.v = killprocess } },
+	{ MODKEY,                       XK_v,      spawn,          {.v = playvideo } },
+	{ MODKEY|ControlMask,           XK_s,      spawn,          {.v = switchoutput } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = ( const char*[] ){ "mpc", "toggle", NULL } } },
 	{ MODKEY,                       XK_c,      spawn,          {.v = ( const char*[] ){ "st", "-e", "calc", NULL } } },
 	{ MODKEY,                       XK_l,      spawn,          {.v = ( const char*[] ){ "st", "-e", "vim", "/home/jules/suckless/dwm/config.h", NULL } } },
